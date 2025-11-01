8aa3b562e1683834b512c4ac567c00b1432670a3
jeramyleon <jeramyleon@gmail.com>
2025-11-01 14:23:33 -0400

    setting up folder structure for backend

---
+++ database/database.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
+const pg = require("pg");
+
+const dbName = "CUNYMarket";
+const db = new Sequelize(
+    process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}`,
+    {
+     logging: false, // comment this line to enable SQL logging   
+    }
+)
